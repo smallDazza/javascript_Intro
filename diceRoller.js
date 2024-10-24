44da5ce7c23a79c65986d0630918f049330418fb
smallDazza <smalldazza@gmail.com>
2024-10-24 14:32:12 +1100

    feat(dice): added callback function examples

--- diceRoller.js
+++ diceRoller.js
@@ -26,6 +26,23 @@ const rollDice15 = () => {
 }
 let diceSide15 = rollDice15();
 console.log(`The side you rolled on the 15 side die is: ${diceSide15} !`)
-
+// If we want more functionality, then we use the function with {}, otherwise we can use this shortened version:
 const rollDice8 = () => rollDice(8);
 console.log("The side you rolled for the 8 sided die is: " + rollDice8())
+
+
+// Callback function ( calling a function within a function):
+function customDiceRoll(side, callback) {
+    return callback(side); // rollDice(30)
+}
+result = customDiceRoll(30, rollDice);
+console.log(result);
+
+//Function to roll multiple dice
+function rollMultipleDice(numdice, diceSize, callback) {
+    for (let i = 0; i < numdice; i++) {
+        const result = rollDice(diceSize);
+        callback(result);
+    }
+}
+rollMultipleDice(3, 6, (result) => console.log("Rolled : " + result));
