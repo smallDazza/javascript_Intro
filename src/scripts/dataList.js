1c5d6ffa353ff604c1a980818fb264f64fbb18cb
smallDazza <smalldazza@gmail.com>
2024-10-29 20:43:52 +1100

    added buttons to the html page

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -11,10 +11,31 @@ let dataArray = [
     "Clowns"
 ];
 
+let costumeContainer = document.getElementById("costumeContainer");
+// Create an unordered list tag
+let costumesContainerList = document.createElement("ul");
+
 dataArray.forEach((costume) => {
+    // Verify what we are working with
     console.log(costume);
+// Create the list element
+    let newCostumeEntry = document.createElement("li");
+ // Add the value to the HTML element
+    newCostumeEntry.innerText = costume;
+// Add a button to each entry to remove it from the list
+    let removeButton = document.createElement("button");
+// Add a placeholder value to button
+    // removeButton.innerText = "Remove" + costume;
+    removeButton.innerText = `Remove ${costume}`;
+
+// Add the element to the container
+    costumesContainerList.appendChild(newCostumeEntry);
+    costumesContainerList.appendChild(removeButton);
 });
 
-let costumeContainer = document.getElementById("costumeContainer");
+//Add  the container to the html page
+costumeContainer.appendChild(costumesContainerList)
 
-costumeContainer.innerText = "Hello World";
+
+
+
